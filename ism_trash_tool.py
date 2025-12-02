2eb65eede48e509f793a782e3cec83d9752b63be
Sohan Subudhi <85711413+SohanSubudhi@users.noreply.github.com>
2025-12-02 02:49:30 -0600

    final deployment bugfix

--- ism_trash_tool.py
+++ ism_trash_tool.py
@@ -1,11 +1,9 @@
 import sys
 import os
 from streamlit.web import cli
+from src import run_app
 
 if __name__ == '__main__':
     # This ensures the current directory is in the python path
     # so that our local imghdr.py can be found.
-    sys.path.insert(0, os.getcwd())
-    
-    sys.argv = ["streamlit", "run", "src/run_app.py"]
-    sys.exit(cli.main())
+    run_app.main()
